3739cf47cc4c9492fd41fd53b11f88958f915125
crr <746730379@qq.com>
2020-07-21 22:02:26 +0800

    第一GIT

---
+++ src/test/java/com/crr/RibbitmqApplicationTests.java
@@ -0,0 +1,37 @@
+package com.crr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbitmqApplicationTests {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @Test
+    void contextLoads() {
+    }
+
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+    @Test
+    public void createExchange(){
+        amqpAdmin.declareExchange(new DirectExchange("amqp.crrexchange"));
+    }
+
+
+    @Test
+    public void createQueue(){
+        amqpAdmin.declareQueue(new Queue("amqp.crrqueue"));
+    }
+    @Test
+    public void bindingQueueAndExchange(){
+        amqpAdmin.declareBinding(new Binding("amqp.crrqueue", Binding.DestinationType.QUEUE,"amqp.crrexchange","amqp.test",null));
+    }
+
+}
